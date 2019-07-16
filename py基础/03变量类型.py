6d8f62c22ec1b990f1000b1399c84972a4f7a0b0
RuoduanOfUbuntu <chad97@126.com>
2019-07-16 18:58:09 +0800

    updata

---
+++ py基础/03变量类型.py
@@ -0,0 +1,23 @@
+#  py是弱类型语言
+#  和大部分语言一样
+
+name = '小明' # str
+age = 18 # int
+sex = True # bool
+height = 1.75 # float
+weight = 65.0 #float
+
+'''
+py 数据类型只分 数字类型 和非数字类型
+整型 int
+浮点型 float
+布尔型 bool
+复数型 complex
+    - 主要用于科学计算
+    
+
+字符串
+列表
+元组
+字典
+'''
