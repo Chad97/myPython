23c6d9b9125866bcd18d238bc2f2876536ac9cc1
ruoduan_mac <chad97@126.com>
2019-10-24 18:57:08 +0800

    submit qrcode print

---
+++ print_qrcode.py
@@ -0,0 +1,7 @@
+import qrcode_terminal
+
+print(' \n \033[1;31;0m  \033[1;31m 在这个1024的特殊日子里 扫描下面👇代码生产的二维码，和我一起守护艾泽拉斯吧～ （长按识别有惊喜哟） \n')
+
+qrcode_terminal.draw('https://www.ruoduan.cn/css/images/wechatImage.jpg')
+
+
